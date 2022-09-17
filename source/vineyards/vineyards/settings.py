d6afeb55009fef20e0bdfee8af63c62edc332460
Polina Chubenko <chubenko.pn@phystech.edu>
2022-09-17 11:04:46 +0300

    index done

--- source/vineyards/vineyards/settings.py
+++ source/vineyards/vineyards/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
-   os.path.join(BASE_DIR, "static"),
+   os.path.join(BASE_DIR, "forecasts/static"),
 ]
 
 STATIC_ROOT = "./collected_static"
