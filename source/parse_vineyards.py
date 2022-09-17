ec4d706440f6c46d80f15b8fcd885da75a408c1d
Polina Chubenko <chubenko.pn@phystech.edu>
2022-09-18 00:16:18 +0300

    grapes variety added to db

--- source/parse_vineyards.py
+++ source/parse_vineyards.py
@@ -1,3 +1,4 @@
+import random
 import sqlite3
 
 # connect to db
@@ -9,27 +10,38 @@ f = open('coordinates.txt', 'r')
 cont = f.read()
 f.close()
 
-# read data from file
+# read names from file
 f = open('names_normalized.txt', 'r')
 names = f.read()
 f.close()
 
 names_list = names.split("\n")
-print(names_list)
+
+# read data absolute from file
+f = open('VINEYARDS.TXT', 'r')
+abs = f.read()
+f.close()
+
+
+grapes_list = ['Саперави', 'Ркацетели', 'Совиньон Блан', 'Красностоп']
 
 # format for inserting to db
 rows = cont.split('\n')
 rows = rows[1:-1]  # delete first row with captions
 formatted = [(x.split(';')[1], x.split(';')[0]) for x in rows]
-print(formatted)
-result = []
 
+# format abs for inserting to db
+rows_abs = abs.split('\n')
+rows_abs = rows_abs[1:-1]  # delete first row with captions
+formatted_abs = [tuple(x.split()[1:]) for x in rows_abs]
+
+result = []
 for i in range(len(formatted)):
-    result += [(*formatted[i], names_list[i])]
+    result += [(*formatted[i], *formatted_abs[i], names_list[i], grapes_list[random.randint(0, 3)])]
 
 print(result)
 
 # insert into db
-cur.executemany("INSERT INTO forecasts_existingvineyard (x, y, name) VALUES (?, ?, ?)", result)
+cur.executemany("INSERT INTO forecasts_existingvineyard (x, y, x_abs, y_abs, name, grape) VALUES (?, ?, ?, ?, ?, ?)", result)
 con.commit()
 con.close()
