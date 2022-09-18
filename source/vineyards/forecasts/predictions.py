468023eda9c25e85ff46a6881fcc7c11496bedd2
DimaSavichev <kubamba2002@gmail.com>
2022-09-18 14:03:09 +0300

    drawing rectangles

--- source/vineyards/forecasts/predictions.py
+++ source/vineyards/forecasts/predictions.py
@@ -4,7 +4,7 @@ from scipy.spatial.distance import cdist
 from pyproj import Proj, transform
 
 x_base = 310050
-y_base = 4800050
+y_base = 4800050 + 4096 * 100
 
 
 def convert_coordinates(x, base):
@@ -15,12 +15,14 @@ def from_picture_to_map(point):
     inProj = Proj("+init=EPSG:32637", preserve_units=True)
     outProj = Proj("+init=EPSG:4326")  # WGS84 in degrees and not  in meters)
     # swap x,y as mkennedy says
-    x1, y1 = point[0] + x_base, point[1] + y_base
+    x1, y1 = point[0] * 100 + x_base, -point[1] * 100 + y_base
     coords = transform(inProj, outProj, x1, y1)
     return coords[1], coords[0]
 
 
 def get_top_vineyard(desired, lands, k=3):
+    desired = desired.drop(["x", "y"], axis=0)
+    lands = lands.drop(["x", "y"], axis=1)
     desired = np.array([np.array(desired, dtype=float)])
     lands = np.array(lands, dtype=float)
     distances = cdist(desired, lands)[0]
@@ -36,13 +38,25 @@ def find_closest(wineyards_x, wineyards_y, k):
     for i in range(len(wineyards_x)):
         s = wineyards.where(wineyards.x == wineyards_x[i]).where(wineyards.y == wineyards_y[i]).iloc[0]
         desired_wineyards = desired_wineyards.append(s)
-    # print(desired_wineyards)
+
     des = desired_wineyards.mean()
     # print(des)
     needed_indices = get_top_vineyard(des, lands, k)
-    ans_x = []
-    ans_y = []
+    areas = pd.read_csv("../land_area.csv")
+    ans = {
+        "x_center": [],
+        "y_center": [],
+        "x_left": [],
+        "x_right": [],
+        "y_top": [],
+        "y_bottom": [],
+    }
     for i in range(k):
-        ans_x.append(lands.iloc[needed_indices[i]].x)
-        ans_y.append(lands.iloc[needed_indices[i]].y)
-    return ans_x, ans_y
+        area = areas[(areas.x == lands.iloc[needed_indices[i]].x) & (areas.y == lands.iloc[needed_indices[i]].y)]
+        ans["x_center"].append(int(area.x))
+        ans["y_center"].append(int(area.y))
+        ans["x_left"].append(int(area.x_left))
+        ans["x_right"].append(int(area.x_right))
+        ans["y_top"].append(int(area.y_top))
+        ans["y_bottom"].append(int(area.y_bottom))
+    return ans
--- source/vineyards/forecasts/static/js/vineyards.js
+++ source/vineyards/forecasts/static/js/vineyards.js
@@ -100,25 +100,14 @@ function init() {
 
     make_list(selected_vineyards);
 
-    for (i = 0; i < result.length; i++) {
+    for (i = 0; i < result["center"].length; i++) {
         console.log("lol");
-        result_placemark = new ymaps.Placemark(
-            [result[i][0], result[i][1]],
-            {
-                iconContent: "Result icon",
-                hintContent: "Result name"
-            },
-            {
-                preset: "islands#blueStretchyIcon",
-                visible: true,
-                // Отключаем кнопку закрытия балуна.
-                balloonCloseButton: false,
-                // Балун будем открывать и закрывать кликом по иконке метки.
-                hideIconOnBalloonOpen: false
-            }
-        );
+        var myRectangle = new ymaps.Rectangle([
+           result["left"][i],
+           result["right"][i]
+        ]);
 
-        myMap.geoObjects.add(result_placemark);
+        myMap.geoObjects.add(myRectangle);
     }
 
     document.getElementById("submit_btn").onclick = function() {
